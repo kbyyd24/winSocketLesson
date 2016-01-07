69f8505ddcec6889a6479c9374704af2a1526322
Ebanks <kbyyd24@outlook.com>
2016-01-07 14:51:31 +0800

    finish server

--- homework/chatServer/chatServer/chatServer.cpp
+++ homework/chatServer/chatServer/chatServer.cpp
@@ -10,39 +10,39 @@
 #pragma comment(lib, "WS2_32.lib")
 
 #define BUF_SIZE 1024
-#define LOGINMSG "OK"
-#define MATCH "MATCH"
-#define IPERROR "IPERROR"
 
 /*struct of online client*/
 typedef struct client {
 	sockaddr_in clientAddr;
 	char sendBuf[BUF_SIZE];
 	//char recvBuf[BUF_SIZE];
-	client* next = NULL;
-	client* pioneer = NULL;
+	client* next;
+	client* pioneer;
 }*pClient;
 
 /*struct of msg send to offline client*/
 typedef struct msgLine {
 	sockaddr_in clientAddr;
 	char buf[BUF_SIZE];
-	msgLine* next = NULL;
+	msgLine* next;
 }*pMsgLine;
 
 sockaddr_in clientAddr;
-pClient clientQueue;
-pClient clientHead;
-pMsgLine msgQueue;
-pMsgLine msgHead;
+pClient clientQueue = NULL;
+pClient clientHead = NULL;
+pMsgLine msgQueue = NULL;
+pMsgLine msgHead = NULL;
 int clientAddrSize = sizeof(clientAddr);
+char recvBuf[BUF_SIZE];
 SOCKET serverSocket;
 
 bool isOnline(SOCKADDR_IN addr) {
-	clientQueue = clientHead;
-	while (clientQueue != NULL) {
-		if (strcmp(inet_ntoa(addr.sin_addr), inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
-			return true;
+	if (clientHead) {
+		clientQueue = clientHead;
+		while (clientQueue != NULL) {
+			if (strcmp(inet_ntoa(addr.sin_addr), inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
+				return true;
+			}
 		}
 	}
 	return false;
@@ -64,11 +64,30 @@ bool isIp(char *ipaddr)
 }
 
 DWORD WINAPI service(LPVOID param) {
-	pClient cli = (pClient)(LPVOID)param;
-	sendto(serverSocket, MATCH, strlen(MATCH), 0, (SOCKADDR*)&clientAddr, clientAddrSize);
-	ZeroMemory(cli->sendBuf, BUF_SIZE);
-	recvfrom(serverSocket, cli->sendBuf, BUF_SIZE, 0, (SOCKADDR*)&clientAddr, &clientAddrSize);
-	sendto(serverSocket, cli->sendBuf, BUF_SIZE, 0, (SOCKADDR*)&clientQueue->clientAddr, sizeof(clientQueue->clientAddr));
+	sockaddr_in cliAddr = *((sockaddr_in*)(LPVOID)param);
+	pClient cli;
+	clientQueue = clientHead;
+	while (clientQueue != NULL) {
+		if (strcmp(inet_ntoa(cliAddr.sin_addr), inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
+			cli = clientQueue;
+		}
+		clientQueue = clientQueue->next;
+	}
+	int cliAddrSize = sizeof(cli->clientAddr);
+	clientQueue = clientHead;
+	while (clientQueue != NULL) {
+		if (strcmp(recvBuf, inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
+			//match client, then send OK msg to the sender
+			ZeroMemory(cli->sendBuf, BUF_SIZE);
+			recvfrom(serverSocket, cli->sendBuf, BUF_SIZE, 0, (SOCKADDR*)&cli->clientAddr, &cliAddrSize);
+			char* msg;
+			sprintf(msg, "%s send to you %s", inet_ntoa(cli->clientAddr.sin_addr), cli->sendBuf);
+			sendto(serverSocket, cli->sendBuf, BUF_SIZE, 0, (SOCKADDR*)&clientQueue->clientAddr, sizeof(clientQueue->clientAddr));
+			break;
+		}
+		clientQueue = clientQueue->next;
+	}
+	
 	return 0;
 }
 
@@ -76,12 +95,6 @@ int main()
 {
 	WSADATA wsaData;
 	sockaddr_in recvAddr;
-	char recvBuf[BUF_SIZE];
-
-	clientQueue = (pClient)malloc(sizeof(client));
-	msgQueue = (pMsgLine)malloc(sizeof(msgLine));
-	clientHead = clientQueue;
-	msgHead = msgQueue;
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		printf("WSAStartup failed with error %d\n", WSAGetLastError());
@@ -94,7 +107,7 @@ int main()
 		return -2;
 	}
 	recvAddr.sin_family = AF_INET;
-	recvAddr.sin_port = htons(10000);
+	recvAddr.sin_port = htons(9990);
 	recvAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
 	if (bind(serverSocket, (sockaddr*)&recvAddr, sizeof(recvAddr)) == SOCKET_ERROR) {
 		printf("bind failed with error %d\n", WSAGetLastError());
@@ -115,8 +128,11 @@ int main()
 				while (clientQueue != NULL) {
 					if (strcmp(recvBuf, inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
 						//match client, then send OK msg to the sender
-						DWORD thId;
-						CreateThread(NULL, NULL, service, (LPVOID)clientQueue, 0, &thId);
+						ZeroMemory(clientQueue->sendBuf, BUF_SIZE);
+						recvfrom(serverSocket, clientQueue->sendBuf, BUF_SIZE, 0, (SOCKADDR*)&clientQueue->clientAddr, &clientAddrSize);
+						char msg[BUF_SIZE];
+						sprintf(msg, "%s send to you %s", inet_ntoa(clientQueue->clientAddr.sin_addr), clientQueue->sendBuf);
+						sendto(serverSocket, clientQueue->sendBuf, BUF_SIZE, 0, (SOCKADDR*)&clientQueue->clientAddr, sizeof(clientQueue->clientAddr));
 						break;
 					}
 					clientQueue = clientQueue->next;
@@ -125,12 +141,13 @@ int main()
 			else if (strcmp(recvBuf, "logout") == 0) {
 				clientQueue = clientHead;
 				while (clientQueue != NULL) {
-					if (strcmp(recvBuf, inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
+					if (strcmp(inet_ntoa(clientAddr.sin_addr), inet_ntoa(clientQueue->clientAddr.sin_addr)) == 0) {
 						//match client, then delete it in online queue
 						if (clientQueue->next == NULL) {
 							if (clientQueue->pioneer == NULL) {
 								free(clientQueue);
-								free(clientHead);
+								clientHead = NULL;
+								clientQueue = clientHead;
 							}
 							else {
 								clientQueue->pioneer->next = NULL;
@@ -147,24 +164,39 @@ int main()
 							clientQueue->next->pioneer = clientQueue->pioneer;
 							free(clientQueue);
 						}
+						break;
 					}
 					clientQueue = clientQueue->next;
 				}
+				printf("client %s logout\n", inet_ntoa(clientAddr.sin_addr));
 			}
 			else {
-				sendto(serverSocket, IPERROR, strlen(IPERROR), 0, (SOCKADDR*)&clientAddr, clientAddrSize);
+				char msg[BUF_SIZE];
+				sprintf(msg, "server: error command: %s", recvBuf);
+				sendto(serverSocket, msg, strlen(msg), 0, (SOCKADDR*)&clientAddr, clientAddrSize);
 			}
 		}
 		else if (strcmp(recvBuf, "login") == 0) {
 			//add to client queue
 			clientQueue = clientHead;
-			while (clientQueue->next != NULL)
-				clientQueue = clientQueue->next;
-			pClient newClient = (pClient)malloc(sizeof(client));
-			clientQueue->next = newClient;
-			newClient->pioneer = clientQueue;
-			newClient->clientAddr = clientAddr;
-			sendto(serverSocket, LOGINMSG, strlen(LOGINMSG), 0, (SOCKADDR*)&(newClient->clientAddr), sizeof(newClient->clientAddr));
+			if (clientHead == NULL) {
+				pClient newClient = (pClient)malloc(sizeof(client));
+				clientQueue = newClient;
+				clientHead = clientQueue;
+				newClient->clientAddr = clientAddr;
+				clientHead->pioneer = NULL;
+				newClient->next = NULL;
+			}
+			else {
+				while (clientQueue->next != NULL)
+					clientQueue = clientQueue->next;
+				pClient newClient = (pClient)malloc(sizeof(client));
+				clientQueue->next = newClient;
+				newClient->pioneer = clientQueue;
+				newClient->clientAddr = clientAddr;
+				newClient->next = NULL;
+			}
+			printf("client %s login\n", inet_ntoa(clientAddr.sin_addr));
 		}
 		else {
 			//add to message queue
